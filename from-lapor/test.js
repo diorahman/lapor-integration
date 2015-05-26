5a913d0e8c1cb4b7ba0ddae176ea650eb33a7b98
Dhi Aurrahman <diorahman@rockybars.com>
2015-05-27 05:20:36 +0700

    Set as text/html

--- from-lapor/test.js
+++ from-lapor/test.js
@@ -2,9 +2,9 @@ var request = require('request');
 var fs = require('fs');
 function test(file) {
   fs.createReadStream(__dirname + '/' + file + '.xml').pipe(request.post({
-    url: 'http://localhost:3000/balas',
+    url: 'http://localhost:3000/notifikasi',
     headers : {
-      'Content-Type': 'application/xml'
+      'Content-Type': 'text/xml'
     }
   }, function(err, res, body){
     console.log(err || body);
